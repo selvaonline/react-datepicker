ffb76cea6610afdd20e6c4476146df817fa1fdfe
Selvakumar Murugesan <selvaonline@gmail.com>
2023-12-15 10:32:01 -0600

    inital draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import MaskedInput from "react-text-mask";
+import createAutoCorrectedDatePipe from "text-mask-addons/dist/createAutoCorrectedDatePipe";
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const autoCorrectedDatePipe = createAutoCorrectedDatePipe("mm/dd/yyyy");
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          dateFormat="MM/dd/yyyy"
+          customInput={
+            <MaskedInput
+              mask={[/\d/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/]}
+              pipe={autoCorrectedDatePipe}
+              keepCharPositions={true}
+              guide={true}
+            />
+          }
+        />
       </header>
     </div>
   );
